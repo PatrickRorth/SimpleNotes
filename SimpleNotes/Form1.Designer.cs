14c3985d9903281af79825eb88219c4d5c2c32f1
Patrick Rørth <pr@kompetera.dk>
2020-11-18 09:44:36 +0100

    Add project files.

---
+++ SimpleNotes/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SimpleNotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(304, 281);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // BottomToolStripPanel
+            // 
+            this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+            this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // TopToolStripPanel
+            // 
+            this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopToolStripPanel.Name = "TopToolStripPanel";
+            this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.RightToolStripPanel.Name = "RightToolStripPanel";
+            this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+            this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Size = new System.Drawing.Size(150, 150);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(304, 281);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "SimpleNotes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+        private System.Windows.Forms.ToolStripContentPanel ContentPanel;
+    }
+}
+
---
+++ SimpleNotes/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleNotes
+{
+    public partial class Form1 : Form
+    {
+
+        string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "SimpleNotes" + ".txt";        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.FromArgb(28, 33, 46);
+            richTextBox1.BackColor = Color.FromArgb(33, 40, 54);
+            richTextBox1.ForeColor = Color.White;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {
+               richTextBox1.LoadFile(path, RichTextBoxStreamType.RichText);
+            }
+            else
+            {
+                richTextBox1.Text = "Features: \n\nCTRL + B = Bold text\nCTRL + S = Save content";
+            }
+            
+        }
+
+        public void saveFile()
+        {
+
+        }
+
+        void KeyPressed (Object sender, KeyEventArgs e)
+        {
+            //IF CTRL + B IS PRESSED - MAKE SELECTE TEXT BOLD
+            if (e.Control && e.KeyCode == Keys.B)
+            {
+                if (richTextBox1.SelectionFont != null)
+                {
+                    System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                    System.Drawing.FontStyle newFontStyle;
+
+                    if (richTextBox1.SelectionFont.Bold == true)
+                    {
+                        newFontStyle = FontStyle.Regular;
+                    }
+                    else
+                    {
+                        newFontStyle = FontStyle.Bold;
+                    }
+
+                    richTextBox1.SelectionFont = new Font(
+                       currentFont.FontFamily,
+                       currentFont.Size,
+                       newFontStyle
+                    );
+                }
+            }
+
+
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                if (richTextBox1.Text != String.Empty)
+                {
+
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                    
+                    if (!File.Exists(path))
+                    {
+                        richTextBox1.SaveFile(path, RichTextBoxStreamType.RichText);
+
+                    }
+                    else
+                    {
+                        richTextBox1.SaveFile(path, RichTextBoxStreamType.RichText);
+                    }
+                    
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("ERROR - Try Again");
+                }
+            }
+
+
+                /*
+                //IF CTRL + I IS PRESSED - MAKE SELECTE TEXT ITALIC
+                if (e.Control && e.KeyCode == Keys.I)
+                {
+                    if (richTextBox1.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle;
+
+                        if (richTextBox1.SelectionFont.Italic == true)
+                        {
+                            newFontStyle = FontStyle.Regular;
+                        }
+                        else
+                        {
+                            newFontStyle = FontStyle.Italic;
+                        }
+
+                        richTextBox1.SelectionFont = new Font(
+                           currentFont.FontFamily,
+                           currentFont.Size,
+                           newFontStyle
+                        );
+                    }
+                }
+
+            
+                //IF CTRL + U IS PRESSED - MAKE SELECTE TEXT UNDERLINE
+                if (e.Control && e.KeyCode == Keys.U)
+                {
+                    if (richTextBox1.SelectionFont != null)
+                    {
+                        System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                        System.Drawing.FontStyle newFontStyle;
+
+                        if (richTextBox1.SelectionFont.Underline == true)
+                        {
+                            newFontStyle = FontStyle.Regular;
+                        }
+                        else
+                        {
+                            newFontStyle = FontStyle.Underline;
+                        }
+
+                        richTextBox1.SelectionFont = new Font(
+                           currentFont.FontFamily,
+                           currentFont.Size,
+                           newFontStyle
+                        );
+                    }
+                }
+                */
+            }
+    }
+}
